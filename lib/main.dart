ad3b6a6e25cfd42dc463dfd67a9681b9d9121b75
CPAYET <cedric.payet@ynov.com>
2022-06-05 00:19:45 +0200

    PageView

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tp_weather/test.dart';
 
 void main() {
   runApp(const MyApp());
@@ -48,68 +49,86 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final PageController controller = PageController();
+  int _page = 1;
 
-  void _incrementCounter() {
+  void _changePageButton(int page) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      int direction = page - _page;
+      controller.animateToPage(controller.page!.toInt() + direction,
+          duration: const Duration(milliseconds: 200), curve: Curves.easeIn);
+      _page += direction;
+    });
+  }
+
+  void _changePageScroll(int page) {
+    setState(() {
+      _page = page + 1;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Column(children: [
+          const Text("Titre"),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width / 3.27,
+                child: TextButton(
+                    onPressed: () => _changePageButton(1),
+                    child: const Text(
+                      "aujourd'hui",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                      ),
+                    )),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width / 3.27,
+                child: TextButton(
+                    onPressed: () => _changePageButton(2),
+                    child: const Text(
+                      "Demain",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                      ),
+                    )),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width / 3.27,
+                child: TextButton(
+                    onPressed: () => _changePageButton(3),
+                    child: const Text(
+                      "10 jours",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                      ),
+                    )),
+              ),
+            ],
+          )
+        ]),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: PageView(
+        controller: controller,
+        onPageChanged: (page) {
+          _changePageScroll(page);
+        },
+        children: [
+          page1(context),
+          page2(context),
+          page3(context),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+Widget page1(BuildContext context) {
+  return const Text("Page 1");
+}
+
+Widget page2(BuildContext context) {
+  return const Text("Page 2");
+}
+
+Widget page3(BuildContext context) {
+  return const Text("Page 3");
+}
